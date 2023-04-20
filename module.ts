642f738223d2ea813f452ebf634bbbf9f20613bc
Changwan Jun <wan2land@gmail.com>
2023-04-21 01:50:12 +0900

    feat: add module

---
+++ module.ts
@@ -0,0 +1,25 @@
+import { MaybePromise } from "./maybe_promise.ts";
+import { ServiceIdentifier } from "./service_identifier.ts";
+import { ConstructType } from "./types.ts";
+
+export interface Module {
+  configure?(descriptor: ModuleDescriptor): void;
+  boot?(descriptor: ModuleDescriptor): MaybePromise<void>;
+  close?(descriptor: ModuleDescriptor): MaybePromise<void>;
+}
+
+export interface ModuleDescriptor {
+  value<T>(id: ServiceIdentifier<T>, value: MaybePromise<T>): void;
+  resolver<T>(id: ServiceIdentifier<T>, resolver: () => MaybePromise<T>): void;
+  bind<T>(constructor: ConstructType<T>): void;
+  bind<T>(
+    id: ServiceIdentifier<T>,
+    constructor: ConstructType<T>,
+  ): void;
+
+  alias<T>(id: ServiceIdentifier<T>, target: ServiceIdentifier<T>): void;
+  resolve<T>(id: ServiceIdentifier<T>): MaybePromise<T>;
+
+  create<T>(ctor: ConstructType<T>): MaybePromise<T>;
+  has<T>(id: ServiceIdentifier<T>): boolean;
+}
