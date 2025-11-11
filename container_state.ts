e512b1a8975aae8abceaaf1ab072dede016476c2
Changwan Jun <hey@wan2.land>
2025-11-11 21:38:06 +0900

    chore: jsdoc @internal

--- container_state.ts
+++ container_state.ts
@@ -1,11 +1,13 @@
 import type { Container } from "./container.ts";
 
+/** @internal */
 export interface ContainerState {
   ref: number;
 }
 
 const containerStateMap = new WeakMap<Container, ContainerState>();
 
+/** @internal */
 export function getContainerState(container: Container): ContainerState {
   let state = containerStateMap.get(container);
   if (!state) {
